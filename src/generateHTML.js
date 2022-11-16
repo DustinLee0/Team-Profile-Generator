f37666ba4ca14900841cc65221a4ab3dd91fb1b9
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-11-15 23:04:46 -0500

    generateHTML.js file finished

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,12 +1,14 @@
 //  create a card for each team member
 function teamCards(team) {
-    //  for each class, return a html card with associated properties
-    team.forEach((obj) => {
+    //  for each class object, return a member card with associated properties
+    //  use .map to return new array of html code of team
+    const cards = team.map((obj) => {
         let role = obj.getRole();
         switch (role) {
             case 'Manager':
-                return `<div class="card">
-                <div class="card-body">
+                return `
+                <div class="card">
+                <div class="card-header-manager">
                   <h4 class="card-title">${obj.getName()}</h4>
                   <h5>${obj.getRole()}</h5>
                 </div>
@@ -15,11 +17,11 @@ function teamCards(team) {
                     <li class="list-group-item">Email: ${obj.getEmail()}</li>
                     <li class="list-group-item">Office #: ${obj.officeNum}</li>
                   </ul>
-              </div>`
-                break;
+                </div>\n`
             case 'Engineer':
-                return `<div class="card">
-                <div class="card-body">
+                return `
+                <div class="card">
+                <div class="card-header-engineer">
                   <h4 class="card-title">${obj.getName()}</h4>
                   <h5>${obj.getRole()}</h5>
                 </div>
@@ -28,11 +30,11 @@ function teamCards(team) {
                     <li class="list-group-item">Email: ${obj.getEmail()}</li>
                     <li class="list-group-item">Github Repository: ${obj.github}</li>
                   </ul>
-              </div>`
-                break;
+                </div>\n`
             case 'Intern':
-                return `<div class="card">
-                <div class="card-body">
+                return `
+                <div class="card">
+                <div class="card-header-intern">
                   <h4 class="card-title">${obj.getName()}</h4>
                   <h5>${obj.getRole()}</h5>
                 </div>
@@ -41,24 +43,34 @@ function teamCards(team) {
                     <li class="list-group-item">Email: ${obj.getEmail()}</li>
                     <li class="list-group-item">School: ${obj.school}</li>
                   </ul>
-              </div>`
-                break;
+                </div>\n`
         }
     })
-    // const teamHTML = team.map( function returnCard(object) {
-    //     return `<div class="card">
-    //     <div class="card-body">
-    //       <h4 class="card-title">${object.getName()}</h4>
-    //       <h5>${object.getRole()}</h5>
-    //     </div>
-    //       <ul class="list-group list-group-flush">
-    //         <li class="list-group-item">Id: ${object.getId()}</li>
-    //         <li class="list-group-item">Email: ${object.getEmail()}</li>
-    //         <li class="list-group-item">Office #: }</li>
-    //       </ul>
-    //   </div>`
-    // })
-    return teamHTML;
+    return cards.join('');
+}
+
+//  generates html code when called
+function generateHTML(team) {
+    const cards = teamCards(team)
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+            <link rel="stylesheet" href="./style.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <h1 class="display-5 text-center header-bg">My Team Profile</h1>
+        <section class="d-flex justify-content-center" id="team-card">\n
+        ${cards}
+        </section>
+    </body>
+    </html>`
 }
 
 module.exports = generateHTML;
